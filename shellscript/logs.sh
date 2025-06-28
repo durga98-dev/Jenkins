c530ecf7fb8bd195e1e987f1a263eacec4d203a5
Durga <durgadevi0707@gmail.com>
2025-06-29 00:20:21 +0530

    committing read-var file

--- shellscript/logs.sh
+++ shellscript/logs.sh
@@ -4,6 +4,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 
+LOG_FOLDER="/var/log/scrip-logs"
+LOG_FILE=$(echo "$0 | cut de "." f1")
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE+$TIMESTAMP.log"
+
 id=$(id -u)
 
 if [ $id -ne 0 ]
@@ -21,23 +26,23 @@ VALIDATE(){
     fi
 }
 
-dnf list installed mysql >> output.log
+dnf list installed mysql &>> $LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
     echo "Install Mysqld"
-    dnf install mysql-server -y >> output.log
+    dnf install mysql-server -y &>> $LOG_FILE_NAME
     VALIDATE $? Mysql Installation # $? = input1 ,Mysql installation = input2  
 else
     echo -e "$Y Mysql already installed....."
 fi
 
-dnf list installed git >> output.log
+dnf list installed git &>> $LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
     echo "Install Git"
-    dnf install git -y >> output.log
+    dnf install git -y &>> $LOG_FILE_NAME
     VALIDATE $? Git Installation # $? = input1 ,Git installation = input2   
 else
     echo -e "$Y Git already installed....."
