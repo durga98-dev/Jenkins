a5b4955de6a9e1492827b46b4e2db7c6f39a120f
Durga <durgadevi0707@gmail.com>
2025-06-28 23:42:02 +0530

    committing read-var file

--- shellscript/colours.sh
+++ shellscript/colours.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#syntax: \e[31m
+#syntax: \e[31m , -e for enabling
 
 #usage
-echo -e "\e[31mHello World"
-echo -e "\e[32mHello World"
-echo -e "\e[33mHello World"
-echo -e "\e[34mHello World"
-echo -e "\e[35mHello World"
-echo -e "\e[36mHello World" #-e for enabling 
+echo -e "\e[31mHello World" #Red
+echo -e "\e[32mHello World" #Green
+echo -e "\e[33mHello World" #Yellow
+echo -e "\e[34mHello World" #Dark Blue
+echo -e "\e[35mHello World" #Purple
+echo -e "\e[36mHello World" #Light blue
+echo -e "\e[37mHello World" # 
