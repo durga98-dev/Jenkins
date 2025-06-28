6ebf512775ea453b43dfe6ef061072b235bbb468
Durga <durgadevi0707@gmail.com>
2025-06-29 00:00:11 +0530

    committing read-var file

---
+++ shellscript/logs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+id=$(id -u)
+
+if [ $id -ne 0 ]
+then 
+   echo -e "$R Error: Must be root user to install the packages"
+   exit 1 # other than 0 as 0 indicates it is success
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+        then
+            echo -e "$2.... is $G SUCCESS"
+        else
+            echo -e "$2.... is $R FAILED"
+    fi
+}
+
+dnf list installed mysql >> output.log
+
+if [ $? -ne 0 ]
+then
+    echo "Install Mysqld"
+    dnf install mysql-server -y >> output.log
+    VALIDATE $? Mysql Installation # $? = input1 ,Mysql installation = input2  
+else
+    echo -e "$Y Mysql already installed....."
+fi
+
+dnf list installed git >> output.log
+
+if [ $? -ne 0 ]
+then
+    echo "Install Git"
+    dnf install git -y >> output.log
+    VALIDATE $? Git Installation # $? = input1 ,Git installation = input2   
+else
+    echo -e "$Y Git already installed....."
+fi
+
