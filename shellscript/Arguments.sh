8da9f75f66ef38aeec17b23f5dde6733e094513f
Durga <durgadevi0707@gmail.com>
2025-06-28 15:50:35 +0530

    Text b/w two friends

---
+++ shellscript/Arguments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+
+PERSON1=$1
+PERSON2=$2
+
+SUM=$(($PERSON1 + $PERSON2))
+echo "sum of two numbers is: $SUM"
--- shellscript/variables.sh
+++ shellscript/variables.sh
@@ -1,7 +1,18 @@
-#!/bin/bash/
+#!/bin/bash
 
-PERSON1=$1
-PERSON2=$2
+VAR1=Durga  #variables are in capital letter and no space before and after =
+VAR2=Kapa
 
-SUM=$($1+$2)
-echo "sum of two numbers is: $SUM"
+echo " $VAR1:: Hi $VAR2"
+echo " $VAR2:: Hi $VAR1"
+echo " $VAR1:: How are you?"
+echo " $VAR2:: I'm good"
+
+
+
+
+#<To overcome this problem variables are defined>
+#echo " Rahim:: Hi Robert"
+#echo " Robert:: Hi Rahim"
+#echo " Rahim:: How are you?"
+#echo " Robert:: I'm good"
