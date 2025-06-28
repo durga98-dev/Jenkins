e174267a5762645d48832062fc57e25030381799
Durga <durgadevi0707@gmail.com>
2025-06-28 23:39:23 +0530

    committing read-var file

---
+++ shellscript/colours.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#syntax: \e[31m
+
+#usage
+echo -e "\e[31mHello World"
+echo -e "\e[32mHello World"
+echo -e "\e[33mHello World"
+echo -e "\e[34mHello World"
+echo -e "\e[35mHello World"
+echo -e "\e[36mHello World" #-e for enabling 
