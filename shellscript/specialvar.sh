c066569f503c0a0ee27f5bd331c89515cc4e7de7
Durga <durgadevi0707@gmail.com>
2025-06-28 17:46:17 +0530

    committing read-var file

---
+++ shellscript/specialvar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+$1, $2, $3 
+echo "All variables passed: $@"
+echo "No.of variables: $#"
+echo "Current script name: $0"
+echo "Present working dire: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user running the script: $USER"
+echo "Process id of current script: $$"
+echo "Process id of last command in background: $!"
+echo "to store the exit status of last command: $?"
+
+
