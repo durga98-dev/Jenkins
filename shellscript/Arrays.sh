00a1d7e41774f7cebde1df25d8937ca652992c8d
Durga <durgadevi0707@gmail.com>
2025-06-28 17:23:02 +0530

    committing read-var file

--- shellscript/Arrays.sh
+++ shellscript/Arrays.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-Toys=("Tom", "Jerry", "Duck", "Elephant")
+Toys=("Tom" "Jerry" "Duck" "Elephant")
 
-for i in Toys 
-do
-echo "Priniting all toy names: $i"
-done
+#index starts from 0 and size is 4
+
+echo "First Toy name is: ${Toys[0]}"
+echo "All toys are: ${Toys[@]}"
+
+# for i in ${Toys[@]}
+# do
+# End=$i
+# done
+# echo "Priniting all toy names: $End "
