25388e5a6f643d4788c9850fe842864dbb7705a4
Durga <durgadevi0707@gmail.com>
2025-06-28 20:35:48 +0530

    committing read-var file

--- shellscript/install-mysql.sh
+++ shellscript/install-mysql.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 id=$(id -u)
-echo "$id"
 
-if [ ]
-dnf install mysql-server -y
+if [ $id -eq 0 ]
+then
+    echo "install Mysqld"
+    dnf install mysql-server -y
+else
+    echo "Error:: Must be root user to install mysql"
+    exit1
+fi
+
+#check if installation is success or not
+
+if [ $? == 0 ]
+then
+    echo "Installation is sucess"
+else
+    echo "Installation is Failed"
