7bfa714b207b21a10906db3eeb1fa5bd26aafaad
Durga <durgadevi0707@gmail.com>
2025-06-29 19:57:04 +0530

    committing for loop

--- shellscript/forloop.sh
+++ shellscript/forloop.sh
@@ -9,7 +9,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 
-LOG_FOLDER="mkdir -p /var/log/scrip-logs"
+LOG_FOLDER=$("mkdir -p /var/log/scrip-logs")
 LOG_FILE=$(echo $0|cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
